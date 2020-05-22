39e16dce48e48bbaa2675168cc36d9a7c186c678
DeveloperPrince <prince.maposa@ciibo.com>
2020-05-22 12:06:41 +0200

    nodejs integration

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+
+const app = express();
 
 //setting middleware
 app.use(express.static(__dirname + '/')); 
 
-var server = app.listen(5000);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'index.html'));
+});
 
+const port = process.env.PORT || 4500;
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
 
 
